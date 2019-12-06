b900873cb4a8238afb9226fdce40b7ac5651071a
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-12-06 04:03:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,9 @@ class Song
  attr_accessor :name, :artist, :genre
  
    @@count = 0
- 
+   @@artists = []
+   @@genres = []
+   
  def initialize(name, artist, genre)
    @@count += 1
    @name = name
