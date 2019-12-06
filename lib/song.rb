b314366e936635ba45f12efdc60b4e5e0b090150
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-12-06 05:25:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,17 +36,17 @@ class Song
     
     
     
-#  def self.genre_count
-#  genre_count = {}
-#  @@genres.each do |genre|
- #   if genre_count[genre]
- #       genre_count[genre] =+ 1
-#    else
- #       genre_count[genre] = 1
- #   end
-#  end
+ def self.genre_count
+   genre_count = {}
+ @@genres.each do |genre|
+    if genre_count[genre]
+       genre_count[genre] =+ 1
+   else
+        genre_count[genre] = 1
+    end
+  end
   
-#genre_count
+genre_count
   
-#end
+end
 end
