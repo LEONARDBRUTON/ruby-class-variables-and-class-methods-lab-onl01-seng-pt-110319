63371ccc056e9ce649727c77c16ca97d75f6ebb5
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-12-06 06:28:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
  end
    
   def self.artist_count
-   artist_count = {}
+   artist_count = nartist_count{}
     @@artists.each do |artist|
     if artist_count[artist]
        artist_count[artist] += 1
