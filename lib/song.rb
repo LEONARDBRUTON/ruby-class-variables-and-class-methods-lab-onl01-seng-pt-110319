3bf303602e2b66b02a4387eb713b2ec1f2e5f0ce
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-12-06 06:53:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,8 +45,6 @@ class Song
    artist_count = Hash.new(0)
     @@artists.each do |artist|
        artist_count[artist] += 1
-    #else
-     #   artist_count[artist] = 1
     end
      artist_count
   end
